bc387365177cf4119af879ef87873cce6f569779
Petar Glusac <glusac-petar@protonmail.com>
2024-04-05 14:47:29 +0200

    Move MoviesMapper to it's own file

---
+++ Movve/Movve/Movies API/MoviesMapper.swift
@@ -0,0 +1,38 @@
+//
+//  MoviesMapper.swift
+//  Movve
+//
+//  Created by Petar Glusac on 5.4.24..
+//
+
+import Foundation
+
+final class MoviesMapper {
+    private struct Root: Decodable {
+        let results: [Item]
+    }
+
+    private struct Item: Decodable {
+        let id: Int
+        let posterPath: String?
+    }
+    
+    private static let OK_200 = 200
+    
+    static func map(_ data: Data, _ response: HTTPURLResponse) -> RemoteMoviesLoader.Result {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        guard response.statusCode == OK_200, let root = try? decoder.decode(Root.self, from: data) else {
+            return .failure(.invalidData)
+        }
+        
+        let movies = root.results.compactMap { item -> Movie? in
+            if let imagePath = item.posterPath {
+                return Movie(id: item.id, imagePath: imagePath)
+            }
+            return nil
+        }
+        return .success(movies)
+    }
+}
--- Movve/Movve/Movies API/RemoteMoviesLoader.swift
+++ Movve/Movve/Movies API/RemoteMoviesLoader.swift
@@ -40,35 +40,3 @@ public final class RemoteMoviesLoader {
         }
     }
 }
-
-final class MoviesMapper {
-    private struct Root: Decodable {
-        let results: [Item]
-    }
-
-    private struct Item: Decodable {
-        let id: Int
-        let posterPath: String?
-    }
-    
-    private static let OK_200 = 200
-    
-    static func map(_ data: Data, _ response: HTTPURLResponse) -> RemoteMoviesLoader.Result {
-        let decoder = JSONDecoder()
-        decoder.keyDecodingStrategy = .convertFromSnakeCase
-        
-        guard response.statusCode == OK_200, let root = try? decoder.decode(Root.self, from: data) else {
-            return .failure(.invalidData)
-        }
-        
-        let movies = root.results.compactMap { item -> Movie? in
-            if let imagePath = item.posterPath {
-                return Movie(id: item.id, imagePath: imagePath)
-            }
-            return nil
-        }
-        return .success(movies)
-    }
-}
-
-
