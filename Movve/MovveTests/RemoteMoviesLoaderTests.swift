51820f88527d677e321de39a9c7e2437b3ab0775
Petar Glusac <glusac-petar@protonmail.com>
2024-04-05 13:51:22 +0200

    Delivers invalid data error on 200 http response with invalid json

--- Movve/MovveTests/RemoteMoviesLoaderTests.swift
+++ Movve/MovveTests/RemoteMoviesLoaderTests.swift
@@ -54,6 +54,15 @@ final class RemoteMoviesLoaderTests: XCTestCase {
         }
     }
     
+    func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
+        let (sut, httpClient) = makeSUT()
+        
+        expect(sut, toCompleteWithError: .invalidData, when: {
+            let invalidJSON = Data("invalid-json".utf8)
+            httpClient.complete(withStatusCode: 200, data: invalidJSON)
+        })
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "https://an-url.com")!) -> (sut: RemoteMoviesLoader, httpClient: HTTPClientSpy) {
@@ -85,14 +94,14 @@ final class RemoteMoviesLoaderTests: XCTestCase {
             messages[index].completion(.failure(error))
         }
         
-        func complete(withStatusCode code: Int, at index: Int = 0) {
+        func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0) {
             let response = HTTPURLResponse(
                 url: requestedURLs[index],
                 statusCode: code,
                 httpVersion: nil,
                 headerFields: nil
             )!
-            messages[index].completion(.success(response))
+            messages[index].completion(.success((data, response)))
         }
     }
 }
