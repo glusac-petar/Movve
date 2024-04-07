36603a5699f879fdbd1ef8f165cbc91f871eec05
Petar Glusac <glusac-petar@protonmail.com>
2024-04-07 23:23:05 +0200

    Delivers failure with error on request error

--- Movve/MovveTests/Movies API/URLSessionHTTPClientTests.swift
+++ Movve/MovveTests/Movies API/URLSessionHTTPClientTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+import Movve
 
 class URLSessionHTTPClient {
     private let session: URLSession
@@ -14,8 +15,12 @@ class URLSessionHTTPClient {
         self.session = session
     }
     
-    func get(from url: URL) {
-        session.dataTask(with: url) { _, _, _ in }.resume()
+    func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
+        session.dataTask(with: url) { _, _, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+        }.resume()
     }
 }
 
@@ -31,7 +36,30 @@ final class URLSessionHTTPClientTests: XCTestCase {
             XCTAssertEqual(request.httpMethod, "GET")
             exp.fulfill()
         }
-        sut.get(from: url)
+        sut.get(from: url) { _ in }
+        
+        wait(for: [exp], timeout: 1.0)
+        URLProtocolStub.stopIntercepting()
+    }
+    
+    func test_getFromURL_failsOnRequestError() {
+        URLProtocolStub.startIntercepting()
+        let url = URL(string: "https://any-url.com")!
+        let sut = URLSessionHTTPClient()
+        let error = NSError(domain: "any-error", code: 1)
+        URLProtocolStub.stub(error: error)
+        
+        let exp = expectation(description: "Wait for completion")
+        sut.get(from: url) { result in
+            switch result {
+            case let .failure(error as NSError):
+                XCTAssertEqual(error.code, error.code)
+                XCTAssertEqual(error.domain, error.domain)
+            default:
+                XCTFail("Expected failure with \(error), got \(result) instead.")
+            }
+            exp.fulfill()
+        }
         
         wait(for: [exp], timeout: 1.0)
         URLProtocolStub.stopIntercepting()
@@ -40,7 +68,16 @@ final class URLSessionHTTPClientTests: XCTestCase {
     // MARK: - Helpers
     
     private final class URLProtocolStub: URLProtocol {
+        private static var stub: Stub?
         private static var requestObserver: ((URLRequest) -> Void)?
+        
+        private struct Stub {
+            let error: Error?
+        }
+        
+        static func stub(error: Error?) {
+            stub = Stub(error: error)
+        }
         
         static func observeRequest(_ observer: @escaping (URLRequest) -> Void) {
             requestObserver = observer
@@ -67,6 +104,10 @@ final class URLSessionHTTPClientTests: XCTestCase {
             if let requestObserver = URLProtocolStub.requestObserver {
                 client?.urlProtocolDidFinishLoading(self)
                 return requestObserver(request)
+            }
+            
+            if let error = URLProtocolStub.stub?.error {
+                client?.urlProtocol(self, didFailWithError: error)
             }
             
             client?.urlProtocolDidFinishLoading(self)
