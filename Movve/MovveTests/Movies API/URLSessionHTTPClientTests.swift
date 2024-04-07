8454aa7c2188e4f020e1b2013a9d763ec49f156e
Petar Glusac <glusac-petar@protonmail.com>
2024-04-07 23:26:32 +0200

    Move SUT creation to the factory method to protect our tests from breaking changes

--- Movve/MovveTests/Movies API/URLSessionHTTPClientTests.swift
+++ Movve/MovveTests/Movies API/URLSessionHTTPClientTests.swift
@@ -37,7 +37,6 @@ final class URLSessionHTTPClientTests: XCTestCase {
     
     func test_getFromURL_performsGETRequestWithURL() {
         let url = URL(string: "https://any-url.com")!
-        let sut = URLSessionHTTPClient()
         
         let exp = expectation(description: "Wait for request")
         URLProtocolStub.observeRequest { request in
@@ -45,19 +44,18 @@ final class URLSessionHTTPClientTests: XCTestCase {
             XCTAssertEqual(request.httpMethod, "GET")
             exp.fulfill()
         }
-        sut.get(from: url) { _ in }
+        makeSUT().get(from: url) { _ in }
         
         wait(for: [exp], timeout: 1.0)
     }
     
     func test_getFromURL_failsOnRequestError() {
         let url = URL(string: "https://any-url.com")!
-        let sut = URLSessionHTTPClient()
         let error = NSError(domain: "any-error", code: 1)
         URLProtocolStub.stub(error: error)
         
         let exp = expectation(description: "Wait for completion")
-        sut.get(from: url) { result in
+        makeSUT().get(from: url) { result in
             switch result {
             case let .failure(error as NSError):
                 XCTAssertEqual(error.code, error.code)
@@ -72,6 +70,11 @@ final class URLSessionHTTPClientTests: XCTestCase {
     }
     
     // MARK: - Helpers
+    
+    private func makeSUT() -> URLSessionHTTPClient {
+        let sut = URLSessionHTTPClient()
+        return sut
+    }
     
     private final class URLProtocolStub: URLProtocol {
         private static var stub: Stub?
