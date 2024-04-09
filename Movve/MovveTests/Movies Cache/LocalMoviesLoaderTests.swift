ceb6d66e99b3d4fe71832f1ac5378094f4ea15f0
Petar Glusac <glusac-petar@protonmail.com>
2024-04-09 13:54:42 +0200

    Save command requests cache deletion

--- Movve/MovveTests/Movies Cache/LocalMoviesLoaderTests.swift
+++ Movve/MovveTests/Movies Cache/LocalMoviesLoaderTests.swift
@@ -6,18 +6,49 @@
 //
 
 import XCTest
+import Movve
 
-class LocalMoviesLoader {}
+class LocalMoviesLoader {
+    private let store: MoviesStore
+    
+    init(store: MoviesStore) {
+        self.store = store
+    }
+    
+    func save(_ movies: [Movie]) {
+        store.deleteCachedMovies()
+    }
+}
 
 class MoviesStore {
     var deleteCachedMoviesCallCount = 0
+    
+    func deleteCachedMovies() {
+        deleteCachedMoviesCallCount += 1
+    }
 }
 
 final class LocalMoviesLoaderTests: XCTestCase {
     func test_init_doesNotDeleteCache() {
         let store = MoviesStore()
-        let _ = LocalMoviesLoader()
+        let _ = LocalMoviesLoader(store: store)
         
         XCTAssertEqual(store.deleteCachedMoviesCallCount, 0)
+    }
+    
+    func test_save_requestsCacheDeletion() {
+        let store = MoviesStore()
+        let sut = LocalMoviesLoader(store: store)
+        let movies = [uniqueMovie(), uniqueMovie()]
+        
+        sut.save(movies)
+        
+        XCTAssertEqual(store.deleteCachedMoviesCallCount, 1)
+    }
+    
+    // MARK: - Helper
+    
+    private func uniqueMovie() -> Movie {
+        return Movie(id: 0, imagePath: UUID().uuidString)
     }
 }
