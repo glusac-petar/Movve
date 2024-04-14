5fb88f1f4f9f469c13877fac9f567f6a28ed3fd1
Petar Glusac <glusac-petar@protonmail.com>
2024-04-14 23:38:20 +0200

    LocalMoviesLoader does not message the store upon creation (before validating the cache)

---
+++ Movve/MovveTests/Movies Cache/ValidateMoviesCacheUseCase.swift
@@ -0,0 +1,84 @@
+//
+//  ValidateMoviesCacheUseCase.swift
+//  MovveTests
+//
+//  Created by Petar Glusac on 14.4.24..
+//
+
+import XCTest
+import Movve
+
+final class ValidateMoviesCacheUseCase: XCTestCase {
+    func test_init_doesNotMessageStore() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+    
+    // MARK: - Helper
+    
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalMoviesLoader, store: MoviesStoreSpy) {
+        let store = MoviesStoreSpy()
+        let sut = LocalMoviesLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+    
+    private final class MoviesStoreSpy: MoviesStore {
+        private var deletionCompletions: [DeletionCompletion] = []
+        private var insertionCompletions: [InsertionCompletion] = []
+        private var retrievalCompletions: [RetrievalCompletion] = []
+        
+        private(set) var receivedMessages: [ReceivedMessage] = []
+        
+        enum ReceivedMessage: Equatable {
+            case deleteCache
+            case insert([LocalMovie], Date)
+            case retrieve
+        }
+        
+        func deleteCachedMovies(completion: @escaping DeletionCompletion) {
+            receivedMessages.append(.deleteCache)
+            deletionCompletions.append(completion)
+        }
+        
+        func completeDeletion(with error: Error, at index: Int = 0) {
+            deletionCompletions[index](error)
+        }
+        
+        func completeDeletionSuccessfully(at index: Int = 0) {
+            deletionCompletions[index](nil)
+        }
+        
+        func insert(_ movies: [LocalMovie], timestamp: Date, completion: @escaping InsertionCompletion) {
+            receivedMessages.append(.insert(movies, timestamp))
+            insertionCompletions.append(completion)
+        }
+        
+        func completeInsertion(with error: Error, at index: Int = 0) {
+            insertionCompletions[index](error)
+        }
+        
+        func completeInsertionSuccessfully(at index: Int = 0) {
+            insertionCompletions[index](nil)
+        }
+        
+        func retrieve(completion: @escaping RetrievalCompletion) {
+            retrievalCompletions.append(completion)
+            receivedMessages.append(.retrieve)
+        }
+        
+        func completeRetrieval(with error: Error, at index: Int = 0) {
+            retrievalCompletions[index](.failure(error))
+        }
+        
+        func completeRetrievalWithEmptyCache(at index: Int = 0) {
+            retrievalCompletions[index](.empty)
+        }
+        
+        func completeRetrieval(with movies: [LocalMovie], timestamp: Date, at index: Int = 0) {
+            retrievalCompletions[index](.found(movies: movies, timestamp: timestamp))
+        }
+    }
+}
