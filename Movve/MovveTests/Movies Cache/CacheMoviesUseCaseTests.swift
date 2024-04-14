4c2b2d74094eb978030a303f68552ce9b1b5adc9
Petar Glusac <glusac-petar@protonmail.com>
2024-04-14 23:36:45 +0200

    Separate LocalMoviesLoaderTests based on use cases

--- Movve/MovveTests/Movies Cache/CacheMoviesUseCaseTests.swift
+++ Movve/MovveTests/Movies Cache/CacheMoviesUseCaseTests.swift
@@ -1,15 +1,14 @@
 //
-//  LocalMoviesLoaderTests.swift
+//  CacheMoviesUseCaseTests.swift
 //  MovveTests
 //
-//  Created by Petar Glusac on 9.4.24..
+//  Created by Petar Glusac on 14.4.24..
 //
 
 import XCTest
 import Movve
 
-final class LocalMoviesLoaderTests: XCTestCase {
-    // MARK: - Save
+final class CacheMoviesUseCaseTests: XCTestCase {
     func test_init_doesNotMessageStore() {
         let (_, store) = makeSUT()
         
@@ -99,134 +98,7 @@ final class LocalMoviesLoaderTests: XCTestCase {
         
         XCTAssertTrue(receivedResults.isEmpty)
     }
-    
-    // MARK: - Load
-    
-    func test_load_requestsCacheRetrieval() {
-        let (sut, store) = makeSUT()
-        
-        sut.load { _ in }
-        
-        XCTAssertEqual(store.receivedMessages, [.retrieve])
-    }
-    
-    func test_load_failsOnRetrievalError() {
-        let (sut, store) = makeSUT()
-        let retrievalError = anyNSError()
-        
-        expect(sut, toCompleteWith: .failure(retrievalError), when: {
-            store.completeRetrieval(with: retrievalError)
-        })
-    }
-    
-    func test_load_deliversNoMoviesOnEmptyCache() {
-        let (sut, store) = makeSUT()
-        
-        expect(sut, toCompleteWith: .success([]), when: {
-            store.completeRetrievalWithEmptyCache()
-        })
-    }
-    
-    func test_load_deliversMoviesOnLessThanSevenDaysOldCache() {
-        let fixedCurrentDate = Date()
-        let lessThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
-        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
-        let movies = uniqueMovies()
-        
-        expect(sut, toCompleteWith: .success(movies.models), when: {
-            store.completeRetrieval(with: movies.local, timestamp: lessThanSevenDaysOldTimestamp)
-        })
-    }
-    
-    func test_load_deliversNoMoviesOnSevenDaysOldCache() {
-        let fixedCurrentDate = Date()
-        let sevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7)
-        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
-        let movies = uniqueMovies()
-        
-        expect(sut, toCompleteWith: .success([]), when: {
-            store.completeRetrieval(with: movies.local, timestamp: sevenDaysOldTimestamp)
-        })
-    }
-    
-    func test_load_deliversNoMoviesOnMoreThanSevenDaysOldCache() {
-        let fixedCurrentDate = Date()
-        let moreThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: -1)
-        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
-        let movies = uniqueMovies()
-        
-        expect(sut, toCompleteWith: .success([]), when: {
-            store.completeRetrieval(with: movies.local, timestamp: moreThanSevenDaysOldTimestamp)
-        })
-    }
-    
-    func test_load_deletesCacheOnRetrievalError() {
-        let (sut, store) = makeSUT(currentDate: Date.init)
-        
-        sut.load { _ in }
-        store.completeRetrieval(with: anyNSError())
-        
-        XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCache])
-    }
-    
-    func test_load_doesNotDeleteCacheOnEmptyCache() {
-        let (sut, store) = makeSUT(currentDate: Date.init)
-        
-        sut.load { _ in }
-        store.completeRetrievalWithEmptyCache()
-        
-        XCTAssertEqual(store.receivedMessages, [.retrieve])
-    }
-    
-    func test_load_doesNotDeleteLessThanSevenDaysOldCache() {
-        let fixedCurrentDate = Date()
-        let lessThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
-        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
-        let movies = uniqueMovies()
-        
-        sut.load { _ in }
-        store.completeRetrieval(with: movies.local, timestamp: lessThanSevenDaysOldTimestamp)
-        
-        XCTAssertEqual(store.receivedMessages, [.retrieve])
-    }
-    
-    func test_load_deletesSevenDaysOldCache() {
-        let fixedCurrentDate = Date()
-        let sevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7)
-        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
-        let movies = uniqueMovies()
-        
-        sut.load { _ in }
-        store.completeRetrieval(with: movies.local, timestamp: sevenDaysOldTimestamp)
-        
-        XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCache])
-    }
-    
-    func test_load_deletesMoreThanSevenDaysOldCache() {
-        let fixedCurrentDate = Date()
-        let moreThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: -1)
-        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
-        let movies = uniqueMovies()
-        
-        sut.load { _ in }
-        store.completeRetrieval(with: movies.local, timestamp: moreThanSevenDaysOldTimestamp)
-        
-        XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCache])
-    }
-    
-    func test_load_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
-        let store = MoviesStoreSpy()
-        var sut: LocalMoviesLoader? = LocalMoviesLoader(store: store, currentDate: Date.init)
-        
-        var receivedResults: [LocalMoviesLoader.LoadResult] = []
-        sut?.load { receivedResults.append($0) }
-        
-        sut = nil
-        store.completeRetrievalWithEmptyCache()
-        
-        XCTAssertTrue(receivedResults.isEmpty)
-    }
-    
+
     // MARK: - Helper
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalMoviesLoader, store: MoviesStoreSpy) {
@@ -241,23 +113,6 @@ final class LocalMoviesLoaderTests: XCTestCase {
         let exp = expectation(description: "Wait for completion")
         sut.save(uniqueMovies().models) { error in
             XCTAssertEqual(error as? NSError, expectedError, file: file, line: line)
-            exp.fulfill()
-        }
-        action()
-        wait(for: [exp], timeout: 1.0)
-    }
-    
-    private func expect(_ sut: LocalMoviesLoader, toCompleteWith expectedResult: LocalMoviesLoader.LoadResult, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
-        let exp = expectation(description: "Wait for completion")
-        sut.load { receivedResult in
-            switch (receivedResult, expectedResult) {
-            case let (.success(receivedMovies), .success(expectedMovies)):
-                XCTAssertEqual(receivedMovies, expectedMovies, file: file, line: line)
-            case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
-                XCTAssertEqual(receivedError, expectedError, file: file, line: line)
-            default:
-                XCTFail("Expected result \(expectedResult), got \(receivedResult) instead.", file: file, line: line)
-            }
             exp.fulfill()
         }
         action()
@@ -333,15 +188,5 @@ final class LocalMoviesLoaderTests: XCTestCase {
     
     private func anyNSError() -> NSError {
         return NSError(domain: "any-error", code: 1)
-    }
-}
-
-private extension Date {
-    func adding(days: Int) -> Date {
-        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
-    }
-    
-    func adding(seconds: TimeInterval) -> Date {
-        return self + seconds
     }
 }
---
+++ Movve/MovveTests/Movies Cache/LoadMoviesFromCacheUseCaseTests.swift
@@ -0,0 +1,250 @@
+//
+//  LoadMoviesFromCacheUseCaseTests.swift
+//  MovveTests
+//
+//  Created by Petar Glusac on 9.4.24..
+//
+
+import XCTest
+import Movve
+
+final class LoadMoviesFromCacheUseCaseTests: XCTestCase {
+    func test_init_doesNotMessageStore() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+    
+    func test_load_requestsCacheRetrieval() {
+        let (sut, store) = makeSUT()
+        
+        sut.load { _ in }
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+    
+    func test_load_failsOnRetrievalError() {
+        let (sut, store) = makeSUT()
+        let retrievalError = anyNSError()
+        
+        expect(sut, toCompleteWith: .failure(retrievalError), when: {
+            store.completeRetrieval(with: retrievalError)
+        })
+    }
+    
+    func test_load_deliversNoMoviesOnEmptyCache() {
+        let (sut, store) = makeSUT()
+        
+        expect(sut, toCompleteWith: .success([]), when: {
+            store.completeRetrievalWithEmptyCache()
+        })
+    }
+    
+    func test_load_deliversMoviesOnLessThanSevenDaysOldCache() {
+        let fixedCurrentDate = Date()
+        let lessThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+        let movies = uniqueMovies()
+        
+        expect(sut, toCompleteWith: .success(movies.models), when: {
+            store.completeRetrieval(with: movies.local, timestamp: lessThanSevenDaysOldTimestamp)
+        })
+    }
+    
+    func test_load_deliversNoMoviesOnSevenDaysOldCache() {
+        let fixedCurrentDate = Date()
+        let sevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7)
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+        let movies = uniqueMovies()
+        
+        expect(sut, toCompleteWith: .success([]), when: {
+            store.completeRetrieval(with: movies.local, timestamp: sevenDaysOldTimestamp)
+        })
+    }
+    
+    func test_load_deliversNoMoviesOnMoreThanSevenDaysOldCache() {
+        let fixedCurrentDate = Date()
+        let moreThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: -1)
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+        let movies = uniqueMovies()
+        
+        expect(sut, toCompleteWith: .success([]), when: {
+            store.completeRetrieval(with: movies.local, timestamp: moreThanSevenDaysOldTimestamp)
+        })
+    }
+    
+    func test_load_deletesCacheOnRetrievalError() {
+        let (sut, store) = makeSUT(currentDate: Date.init)
+        
+        sut.load { _ in }
+        store.completeRetrieval(with: anyNSError())
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCache])
+    }
+    
+    func test_load_doesNotDeleteCacheOnEmptyCache() {
+        let (sut, store) = makeSUT(currentDate: Date.init)
+        
+        sut.load { _ in }
+        store.completeRetrievalWithEmptyCache()
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+    
+    func test_load_doesNotDeleteLessThanSevenDaysOldCache() {
+        let fixedCurrentDate = Date()
+        let lessThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+        let movies = uniqueMovies()
+        
+        sut.load { _ in }
+        store.completeRetrieval(with: movies.local, timestamp: lessThanSevenDaysOldTimestamp)
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+    
+    func test_load_deletesSevenDaysOldCache() {
+        let fixedCurrentDate = Date()
+        let sevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7)
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+        let movies = uniqueMovies()
+        
+        sut.load { _ in }
+        store.completeRetrieval(with: movies.local, timestamp: sevenDaysOldTimestamp)
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCache])
+    }
+    
+    func test_load_deletesMoreThanSevenDaysOldCache() {
+        let fixedCurrentDate = Date()
+        let moreThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: -1)
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+        let movies = uniqueMovies()
+        
+        sut.load { _ in }
+        store.completeRetrieval(with: movies.local, timestamp: moreThanSevenDaysOldTimestamp)
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCache])
+    }
+    
+    func test_load_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
+        let store = MoviesStoreSpy()
+        var sut: LocalMoviesLoader? = LocalMoviesLoader(store: store, currentDate: Date.init)
+        
+        var receivedResults: [LocalMoviesLoader.LoadResult] = []
+        sut?.load { receivedResults.append($0) }
+        
+        sut = nil
+        store.completeRetrievalWithEmptyCache()
+        
+        XCTAssertTrue(receivedResults.isEmpty)
+    }
+    
+    // MARK: - Helper
+    
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalMoviesLoader, store: MoviesStoreSpy) {
+        let store = MoviesStoreSpy()
+        let sut = LocalMoviesLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+    
+    private func expect(_ sut: LocalMoviesLoader, toCompleteWith expectedResult: LocalMoviesLoader.LoadResult, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
+        let exp = expectation(description: "Wait for completion")
+        sut.load { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedMovies), .success(expectedMovies)):
+                XCTAssertEqual(receivedMovies, expectedMovies, file: file, line: line)
+            case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
+                XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+            default:
+                XCTFail("Expected result \(expectedResult), got \(receivedResult) instead.", file: file, line: line)
+            }
+            exp.fulfill()
+        }
+        action()
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    private final class MoviesStoreSpy: MoviesStore {
+        private var deletionCompletions: [DeletionCompletion] = []
+        private var insertionCompletions: [InsertionCompletion] = []
+        private var retrievalCompletions: [RetrievalCompletion] = []
+        
+        private(set) var receivedMessages: [ReceivedMessage] = []
+        
+        enum ReceivedMessage: Equatable {
+            case deleteCache
+            case insert([LocalMovie], Date)
+            case retrieve
+        }
+        
+        func deleteCachedMovies(completion: @escaping DeletionCompletion) {
+            receivedMessages.append(.deleteCache)
+            deletionCompletions.append(completion)
+        }
+        
+        func completeDeletion(with error: Error, at index: Int = 0) {
+            deletionCompletions[index](error)
+        }
+        
+        func completeDeletionSuccessfully(at index: Int = 0) {
+            deletionCompletions[index](nil)
+        }
+        
+        func insert(_ movies: [LocalMovie], timestamp: Date, completion: @escaping InsertionCompletion) {
+            receivedMessages.append(.insert(movies, timestamp))
+            insertionCompletions.append(completion)
+        }
+        
+        func completeInsertion(with error: Error, at index: Int = 0) {
+            insertionCompletions[index](error)
+        }
+        
+        func completeInsertionSuccessfully(at index: Int = 0) {
+            insertionCompletions[index](nil)
+        }
+        
+        func retrieve(completion: @escaping RetrievalCompletion) {
+            retrievalCompletions.append(completion)
+            receivedMessages.append(.retrieve)
+        }
+        
+        func completeRetrieval(with error: Error, at index: Int = 0) {
+            retrievalCompletions[index](.failure(error))
+        }
+        
+        func completeRetrievalWithEmptyCache(at index: Int = 0) {
+            retrievalCompletions[index](.empty)
+        }
+        
+        func completeRetrieval(with movies: [LocalMovie], timestamp: Date, at index: Int = 0) {
+            retrievalCompletions[index](.found(movies: movies, timestamp: timestamp))
+        }
+    }
+    
+    private func uniqueMovie() -> Movie {
+        return Movie(id: 0, imagePath: UUID().uuidString)
+    }
+    
+    private func uniqueMovies() -> (models: [Movie], local: [LocalMovie]) {
+        let movies = [uniqueMovie(), uniqueMovie()]
+        let local = movies.map { LocalMovie(id: $0.id, imagePath: $0.imagePath) }
+        return (movies, local)
+    }
+    
+    private func anyNSError() -> NSError {
+        return NSError(domain: "any-error", code: 1)
+    }
+}
+
+private extension Date {
+    func adding(days: Int) -> Date {
+        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+    }
+    
+    func adding(seconds: TimeInterval) -> Date {
+        return self + seconds
+    }
+}
