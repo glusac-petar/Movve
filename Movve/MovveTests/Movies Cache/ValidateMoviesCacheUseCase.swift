03f78a83ef829e49ec7a12780110043710ee71cb
Petar Glusac <glusac-petar@protonmail.com>
2024-04-14 23:56:28 +0200

    Move MoviesStoreSpy to shared helpers scope

--- Movve/MovveTests/Movies Cache/ValidateMoviesCacheUseCase.swift
+++ Movve/MovveTests/Movies Cache/ValidateMoviesCacheUseCase.swift
@@ -93,63 +93,6 @@ final class ValidateMoviesCacheUseCase: XCTestCase {
         return (sut, store)
     }
     
-    private final class MoviesStoreSpy: MoviesStore {
-        private var deletionCompletions: [DeletionCompletion] = []
-        private var insertionCompletions: [InsertionCompletion] = []
-        private var retrievalCompletions: [RetrievalCompletion] = []
-        
-        private(set) var receivedMessages: [ReceivedMessage] = []
-        
-        enum ReceivedMessage: Equatable {
-            case deleteCache
-            case insert([LocalMovie], Date)
-            case retrieve
-        }
-        
-        func deleteCachedMovies(completion: @escaping DeletionCompletion) {
-            receivedMessages.append(.deleteCache)
-            deletionCompletions.append(completion)
-        }
-        
-        func completeDeletion(with error: Error, at index: Int = 0) {
-            deletionCompletions[index](error)
-        }
-        
-        func completeDeletionSuccessfully(at index: Int = 0) {
-            deletionCompletions[index](nil)
-        }
-        
-        func insert(_ movies: [LocalMovie], timestamp: Date, completion: @escaping InsertionCompletion) {
-            receivedMessages.append(.insert(movies, timestamp))
-            insertionCompletions.append(completion)
-        }
-        
-        func completeInsertion(with error: Error, at index: Int = 0) {
-            insertionCompletions[index](error)
-        }
-        
-        func completeInsertionSuccessfully(at index: Int = 0) {
-            insertionCompletions[index](nil)
-        }
-        
-        func retrieve(completion: @escaping RetrievalCompletion) {
-            retrievalCompletions.append(completion)
-            receivedMessages.append(.retrieve)
-        }
-        
-        func completeRetrieval(with error: Error, at index: Int = 0) {
-            retrievalCompletions[index](.failure(error))
-        }
-        
-        func completeRetrievalWithEmptyCache(at index: Int = 0) {
-            retrievalCompletions[index](.empty)
-        }
-        
-        func completeRetrieval(with movies: [LocalMovie], timestamp: Date, at index: Int = 0) {
-            retrievalCompletions[index](.found(movies: movies, timestamp: timestamp))
-        }
-    }
-    
     private func uniqueMovie() -> Movie {
         return Movie(id: 0, imagePath: UUID().uuidString)
     }
