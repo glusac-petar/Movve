c946d607037d461eb061e648ab74a38fb1d4e36c
Petar Glusac <glusac-petar@protonmail.com>
2024-04-23 00:04:39 +0200

    Add helper methods to provide documentation (context), and to clarify test `setUp` and `tearDown` intent regarding side-effects

--- Movve/MovveTests/Movies Cache/CodableMoviesStoreTests.swift
+++ Movve/MovveTests/Movies Cache/CodableMoviesStoreTests.swift
@@ -60,12 +60,12 @@ class CodableMoviesStore {
 final class CodableMoviesStoreTests: XCTestCase {
     override func setUp() {
         super.setUp()
-        try? FileManager.default.removeItem(at: testSpecificStoreURL())
+        setupEmptyStoreState()
     }
     
     override func tearDown() {
         super.tearDown()
-        try? FileManager.default.removeItem(at: testSpecificStoreURL())
+        undoStoreSideEffects()
     }
     
     func test_retrieve_deliversEmptyOnEmptyCache() {
@@ -134,6 +134,18 @@ final class CodableMoviesStoreTests: XCTestCase {
         let sut = CodableMoviesStore(storeURL: testSpecificStoreURL())
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
+    }
+    
+    private func setupEmptyStoreState() {
+        deleteStoreArtifacts()
+    }
+    
+    private func undoStoreSideEffects() {
+        deleteStoreArtifacts()
+    }
+    
+    private func deleteStoreArtifacts() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
     }
     
     private func testSpecificStoreURL() -> URL {
